8fba12cdeff577d731045b41b7852333611640de
Cydev2306 <charlier.cy@gmail.com>
2016-06-13 17:45:02 +0200

    fix bugs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.disable('view cache');
 // uncomment after placing your favicon in /public
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,17 +21,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/static', express.static('./public'));
 
 app.use('/', routes);
 app.use('/mailer',mailer);
 app.use('/contact',contact);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+});*/
 
 // error handlers
 
@@ -58,5 +59,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(3000, function(){
+  console.log('app started');
+});
 
-module.exports = app;
+//module.exports = app;
