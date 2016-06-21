33a0cd099189a67a10733249e82fd49f2cf54e27
Cydev2306 <charlier.cy@gmail.com>
2016-06-21 10:11:06 +0200

    fix design

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,12 +1,12 @@
-/**	
+/**
 	* Template Name: App Landy
-	* Version: 1.0	
+	* Version: 1.0
 	* Template Scripts
 	* Author: MarkUps
 	* Author URI: http://www.markups.io/
 
 	Custom JS
-	
+
 	1. MENU SLIDE
 	2. MENU SMOOTH SCROLLING
 	3. SCREEN SLIDER (SLICK SLIDER)
@@ -14,44 +14,44 @@
 	5. TESTIMONIAL SLIDE (SLICK SLIDER)
 	6. PRELOADER
 	7. WOW ANIMATION
-	8. MOBILE MENU CLOSE 
-	9. PRELOADER 
+	8. MOBILE MENU CLOSE
+	9. PRELOADER
 	10. INSTAGRAM SLIDER (SLICK SLIDER)
-	11. WOW ANIMATION 	
-	
+	11. WOW ANIMATION
+
 **/
 
 jQuery(function($){
 
 	/* ----------------------------------------------------------- */
 	/*  1. MENU SLIDE
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	var menuRight = document.getElementById( 'main-menu' ),
 		showRight = document.getElementById( 'menu-btn' ),
-		close = document.getElementById( 'close' ),								
+		close = document.getElementById( 'close' ),
 		body = document.body;
 
 	window.onload = function() {
 		showRight.onclick = function(e) {
 			e.preventDefault();
 			classie.toggle( this, 'active' );
-			classie.toggle( menuRight, 'menu-open' );			
+			classie.toggle( menuRight, 'menu-open' );
 		};
 
 		close.onclick = function() {
-			menuRight.hide();						
+			menuRight.hide();
 		};
 
 		menuRight.onclick = function() {
 			classie.toggle( this, 'active' );
-			classie.toggle( menuRight, 'menu-open' );				
+			classie.toggle( menuRight, 'menu-open' );
 		};
 	};
-			
+
 	/* ----------------------------------------------------------- */
 	/*  2. MENU SMOOTH SCROLLING
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	//MENU SCROLLING WITH ACTIVE ITEM SELECTED
 
@@ -68,7 +68,7 @@ jQuery(function($){
 		$('html,body').animate({scrollTop:dest}, 1000,'swing');
     });
 
-	
+
 
 	/* ----------------------------------------------------------- */
 	/*  3. SCREEN SLIDER (SLICK SLIDER)
@@ -77,9 +77,9 @@ jQuery(function($){
 	jQuery('.screenshots-slide').slick({
 	  dots: true,
 	  infinite: true,
-	  arrows:true, 
+	  arrows:true,
  	  centerMode: true,
-  	  centerPadding: '60px',  
+  	  centerPadding: '60px',
 	  slidesToShow: 3,
 	  responsive: [
 	    {
@@ -112,12 +112,12 @@ jQuery(function($){
 	});
 
 	/* ----------------------------------------------------------- */
-	/*  4. FANCYBOX 
+	/*  4. FANCYBOX
 	/* ----------------------------------------------------------- */
 
 	jQuery(document).ready(function() {
 		$(".fancybox").fancybox();
-	});	 
+	});
 
 	/* ----------------------------------------------------------- */
 	/*  5. TESTIMONIAL SLIDE(SLICK SLIDER)
@@ -128,38 +128,35 @@ jQuery(function($){
 		infinite: true,
 		speed: 500,
 		fade: true,
-		arrows:false, 
-		autoplay: true,     
+		arrows:false,
+		autoplay: true,
       	slidesToShow: 1,
-		slide: 'div',		
+		slide: 'div',
 		cssEase: 'linear'
 	});
-	
+
 
 	/* ----------------------------------------------------------- */
-	/*  6. PRELOADER 
-	/* ----------------------------------------------------------- */ 
-	
+	/*  6. PRELOADER
+	/* ----------------------------------------------------------- */
+
 	jQuery(window).load(function() { // makes sure the whole site is loaded
       $('#status').fadeOut(); // will first fade out the loading animation
       $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
       $('body').delay(100).css({'overflow':'visible'});
     })
 
-   
+
 	/* ----------------------------------------------------------- */
 	/*  7. WOW ANIMATION
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	wow = new WOW(
       {
         animateClass: 'animated',
-        offset:       100,
-        callback:     function(box) {
-          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-        }
+        offset:       100
       }
     );
     wow.init();
-	
+
 });
